c2b115c9402f070159a1e0de7a155a61b920450b
zielnik <zielnik@gmail.com>
2017-05-31 22:32:29 +0200

    Add files via upload

---
+++ test.psm1
@@ -0,0 +1,52 @@
+﻿function Get-SomeCompsInfo {
+    <#
+    .SYNOPSIS
+    Basic function to gather some WMI info
+    .DESCRIPTION
+    Basic function to gather some WMI info using CIM session to remote host
+    .NOTES
+    Author:    Krzysztof Zielinski
+    Version:   0.01
+    #>
+    [CmdletBinding()]
+    param(
+        [parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias('cn','Machine')]
+        [string[]]$ComputerName,
+        [switch]$AlternateCredentials
+    )
+    Begin {
+        if ($AlternateCredentials -eq $true) {
+            $Creds = Get-Credential 
+        }
+    }
+    Process{
+        foreach ($Computer in $ComputerName) {
+            try{
+                if ($Creds -ne $null) {
+                    $sess = New-CimSession -ComputerName $Computer -Credential $Creds -ErrorAction Stop -ErrorVariable MyErr
+                } else {
+                    $sess = New-CimSession -ComputerName $Computer -ErrorAction Stop -ErrorVariable MyErr
+                }
+                $CS = Get-CimInstance -CimSession $sess -ClassName win32_OperatingSystem
+                $props = [ordered]@{
+                                    'CSCaption' = $CS.Caption
+                                    'Manufacturer' = $CS.Manufacturer
+                                    'SN' = $CS.SerialNumber
+                                  }
+                $out = New-Object -TypeName psobject -Property $props
+                Write-Output $out
+            } catch {
+                Write-Warning "$($MyErr.ErrorRecord)"
+            }
+        }
+    }
+    End {
+
+    }
+}
+
